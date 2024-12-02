3ae67b1d53f044a4358fb11437661ecc2da42f07
tunglamp08072005 <tunglamp227@gmail.com>
2024-12-02 22:04:11 +0700

    start

---
+++ src/main/java/org/example/coffeeshop/MenuItem.java
@@ -0,0 +1,75 @@
+package org.example.coffeeshop;
+
+public class MenuItem {
+    private int id;
+    private String name;
+    private String type;
+    private double price;
+    private String description;
+    private boolean availability;
+
+    public MenuItem(int id, String name, String type, double price, String description, boolean availability) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+        this.description = description;
+        this.availability = availability;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuItem{id=" + id + ", name=" + name + '\'' +
+                ", type=" + type + '\'' + ", price=" + price +
+                ", description=" + description + '\'' +
+                ", availability=" + (availability ? "Available" : "Not Available") + '}';
+    }
+}
