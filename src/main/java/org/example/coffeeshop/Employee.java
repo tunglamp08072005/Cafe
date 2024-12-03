9e53993f35a7dcc2def0f71395ea9fd21c07f9fc
tunglamp08072005 <tunglamp227@gmail.com>
2024-12-03 08:31:17 +0700

    Employee

---
+++ src/main/java/org/example/coffeeshop/Employee.java
@@ -0,0 +1,54 @@
+package org.example.coffeeshop;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String position;
+    private double salary;
+
+    public Employee(int id, String name, String position, double salary) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{id=" + id + ", name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", salary=" + salary + '}';
+    }
+}
