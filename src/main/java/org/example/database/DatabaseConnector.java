2656327ae958f01a9b82a238798f1f4739d1706d
tunglamp08072005 <tunglamp227@gmail.com>
2024-12-06 08:47:06 +0700

    tao database

---
+++ src/main/java/org/example/database/DatabaseConnector.java
@@ -0,0 +1,19 @@
+package org.example.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    private static final String URL = "jdbc:mysql://localhost:3307/coffee_shop";
+    private static final String USER = "root";
+    private static final String PASSWORD = "A3_lamsoaica";
+
+    private DatabaseConnector() {
+
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
